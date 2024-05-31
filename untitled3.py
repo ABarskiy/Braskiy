70f014ce760f0afcc585a51e7e90166ca64c2283
ABarskiy <ivanivanowvv1043@gmail.com>
2024-05-31 18:29:39 +0300

    Update untitled3.py

--- untitled3.py
+++ untitled3.py
@@ -95,9 +95,11 @@ class Macbook(Computer):
         self.graphics_card = graphics_card
         self.processor = processor
         self.ram = ram
+    def make_sound(self):
+        print(f"{self.name} play sound ")
 
 f = Macbook("Apple Macbookpro 13", "m1", 2021, "1.25 kg", "8 hours", GraphicsCard("M1 Pro 14-Core GPU", "16 GB", "3.2 GHz"), Processor("Apple M1", 8, "3.2 GHz"), Ram("16 GB", "3200 MHz"))
-g = Macbook("Apple Macbookpro 13", "m1", 2021, "2 kg", "16 hours", GraphicsCard("M2 Pro 14-Core GPU", "32 GB", "5 GHz"), Processor("Apple M2", 16, "4 GHz"), Ram("32 GB", "4600 MHz"))
+g = Macbook("Apple Macbookpro 15", "m1", 2022, "2 kg", "16 hours", GraphicsCard("M2 Pro 14-Core GPU", "32 GB", "5 GHz"), Processor("Apple M2", 16, "4 GHz"), Ram("32 GB", "4600 MHz"))
 
 def compare_components(f, g):
     print("Weight:")
@@ -118,9 +120,9 @@ def compare_components(f, g):
 
     print("\nGraphics Card Clock Speed:")
     if f.graphics_card.get_clock_speed() > g.graphics_card.get_clock_speed():
-        print(f"{f.graphics_card.get_clock_speed()} GHz < {g.graphics_card.get_clock_speed()} GHz")
-    elif f.graphics_card.get_clock_speed() < g.graphics_card.get_clock_speed():
         print(f"{f.graphics_card.get_clock_speed()} GHz > {g.graphics_card.get_clock_speed()} GHz")
+    elif f.graphics_card.get_clock_speed() < g.graphics_card.get_clock_speed():
+        print(f"{f.graphics_card.get_clock_speed()} GHz < {g.graphics_card.get_clock_speed()} GHz")
     else:
         print(f"{f.graphics_card.get_clock_speed()} GHz == {g.graphics_card.get_clock_speed()} GHz")
     
@@ -134,10 +136,11 @@ def compare_components(f, g):
 
     print("\nRAM Speed:")
     if f.ram.get_speed() > g.ram.get_speed():
-        print(f"{f.ram.get_speed()} MHz < {g.ram.get_speed()} MHz")
-    elif f.ram.get_speed() < g.ram.get_speed():
         print(f"{f.ram.get_speed()} MHz > {g.ram.get_speed()} MHz")
+    elif f.ram.get_speed() < g.ram.get_speed():
+        print(f"{f.ram.get_speed()} MHz < {g.ram.get_speed()} MHz")
     else:
         print(f"{f.ram.get_speed()} MHz == {g.ram.get_speed()} MHz")
 
 compare_components(f, g)
+print("\nИсходя из сравнения, Macbook Pro 15 лучше, чем Macbook Pro 13, а именоо: батреей на 16 часов работы, Ghz видеокарты, Ghz процессора и Ghz ОЗУ ")
